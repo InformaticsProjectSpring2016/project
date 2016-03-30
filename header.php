a4f74ad7ecd3ec9bb553a80c49543c4ef3066fe9
EmmaClingman <emmaclingman@gmail.com>
2016-03-30 10:01:27 -0500

    Update header.php

--- header.php
+++ header.php
@@ -26,23 +26,62 @@ include_once("UTILITIES/config.php");
 
 
 <body>
+	<h2> &nbsp&nbsp Anti Wage Theft </h2>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+			<li><a href="#">My Data</a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">One more separated link</a></li>
+			  </ul>
+			</li>
+		  </ul>
+		  
+		  <ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Link</a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+			  </ul>
+			</li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
 	<!-- Beginning of bootstrap -->
-	<div class = "container">
+	
+	<!-- <div class = "container">
 		<!-- header -->
-		<div class = "page-header">
+		<!--<div class = "page-header">
 			<ul class="nav nav-pills pull-right">
 				<li role="presentation" <?php if($menuHighlight == 0) { echo 'class="active"';} ?>><a href=<?php echo '"'.$BaseUrl.'"';?>">Home</a></li>
 				
-				<?php
-					if(!$loggedIn){
-						echo '
-						<a href="' . $BaseUrl . '/login.php" class="btn btn-success" role="button">Login</a>
-						<a href="' . $BaseUrl . '/register.php" class="btn btn-info" role="button">Register</a>';
-					} 
-					else {
-						echo '<a href="' . $BaseUrl . '/logout.php" class="btn btn-warning" role="button">Logout</a>';
-					}
-				?>
+				// <?php 
+					//if(!$loggedIn){
+						// echo '
+						// <a href="' . $BaseUrl . '/login.php" class="btn btn-success" role="button">Login</a>
+						// <a href="' . $BaseUrl . '/register.php" class="btn btn-info" role="button">Register</a>';
+					// }
+					// else {
+						// echo '<a href="' . $BaseUrl . '/logout.php" class="btn btn-warning" role="button">Logout</a>';
+					// }
+				// ?>
 			</ul>
 			<h2>Anti Wage Theft</h2>
 		</div> <!-- Closing Header-->
