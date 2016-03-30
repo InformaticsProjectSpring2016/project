9f14fe8691461efcfa51f0653f4303c96840e633
EmmaClingman <emmaclingman@gmail.com>
2016-03-30 12:15:45 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 	<!--footer-->
 	<div class ="navbar navbar-default navbar-fixed-bottom">
+		<li class="active"><a href="#">Administrator</a></li>
 		<div class = "container">
 			<p class="text-center text-muted"><span class="glyphicon glyphicon-copyright-mark"></span> Copyright 2016 - Nate Gramer, Emma Clingman, Tian Yu</p>
 		</div>
