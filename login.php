65aeadc1bde567127e7d21e4887c62e11df2ff8e
EmmaClingman <emmaclingman@gmail.com>
2016-03-30 13:32:24 -0500

    Update login.php

--- login.php
+++ login.php
@@ -6,33 +6,33 @@
 
 
 <div class = "col-xs-2"></div> <!-- Used to push jumbotron smaller and to the right -->
-<div class = "col-xs-8">
-<div class="text-center">
-	<!-- jumbotron-->
-	<div class="jumbotron">
+	<div class = "col-xs-8">
+	<div class="text-center">
+		<!-- jumbotron-->
+		<div class="jumbotron">
 
-		<div class="text-center">
-			<h1>Login</h1>
-			<p class="lead">Please enter your username and password below.</p>
-			<div class="form-group">
-				<!--<label for="username">Username</label>-->
-				<div class="input-group">	
-					<div class="input-group-addon">Username</div>
-					<input type="text" class="form-control" placeholder="Username" name="username"/>
+			<div class="text-center">
+				<h1>Login</h1>
+				<p class="lead">Please enter your username and password below.</p>
+				<div class="form-group">
+					<!--<label for="username">Username</label>-->
+					<div class="input-group">	
+						<div class="input-group-addon">Username</div>
+						<input type="text" class="form-control" placeholder="Username" name="username"/>
+					</div>
 				</div>
-			</div>
-				
-			<div class="form-group">
-				<!--<label for="password">Password</label>-->
-				<div class="input-group">
-					<div class="input-group-addon">Password</div>
-					<input type="password" class="form-control" placeholder="Password" name="password"/>
+					
+				<div class="form-group">
+					<!--<label for="password">Password</label>-->
+					<div class="input-group">
+						<div class="input-group-addon">Password</div>
+						<input type="password" class="form-control" placeholder="Password" name="password"/>
+					</div>
 				</div>
+			<button type="submit" class="btn btn-success btn-lg" name="submit">Login</button>
 			</div>
-		<button type="submit" class="btn btn-success btn-lg" name="submit">Login</button>
-		</div>
-	</div> <!-- Jumbotron -->
-</div>
+		</div> <!-- Jumbotron -->
+	</div>
 </div>
 
 <?php
