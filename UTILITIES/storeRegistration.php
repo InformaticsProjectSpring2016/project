593ecb89adb2bce4778ef87c93a0c9ac47e7b127
@nate-gramer <nathan-gramer@uiowa.edu>
2016-03-30 22:41:16 -0500

    Made it work

--- UTILITIES/storeRegistration.php
+++ UTILITIES/storeRegistration.php
@@ -7,18 +7,18 @@
 	$Username = mysqli_real_escape_string($db, $_POST['Username']);
 	$Firstname = mysqli_real_escape_string($db, $_POST['Firstname']);
 	$Lastname = mysqli_real_escape_string($db, $_POST['Lastname']);
-	$Password = mysqli_real_escape_string($db, $_POST['Password']);
+	$Password = mysqli_real_escape_string($db, $_POST['Password1']);
 	$Cell = mysqli_real_escape_string($db, $_POST['Cell']);
 	$Age = mysqli_real_escape_string($db, $_POST['Age']);
 	$Employer = mysqli_real_escape_string($db, $_POST['Employer']);
-
+	$Email = mysqli_real_escape_string($db, $_POST['Email']);
 	$Password = crypt($Password);
 	// WILL NEED TO ADD EMPLOYER INTO EMPLOYERS TABLE IF DOESNT EXIST
-	$query = "insert into Users (Username, Firstname, Lastname, UserPassword, Phone, Age) VALUES ('$Username','$Firstname','$Lastname','$Password','$Cell','$Age');";
+	$query = "insert into Users (Username, FirstName, LastName, UserPassword, Phone, Age,Email) VALUES ('$Username','$Firstname','$Lastname','$Password','$Cell','$Age','$Email');";
 	$result = queryDB($query, $db);
 	if ($result) {
-		echo "You were added to the database. Please login now.";
-		include_once("login.php");
+		echo "You were successfully registered! Please login now.";
+		header('Refresh: 2; url=../login.php');
 	} else {
 		echo "something went wrong";
 		die();
