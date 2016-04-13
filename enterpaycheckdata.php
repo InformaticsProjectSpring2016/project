08d274be615c8f1a2af05ffeda1f99cc84de4965
@nate-gramer <nathan-gramer@uiowa.edu>
2016-04-13 16:31:25 -0500

    Made this page have datepickers, works fully

--- enterpaycheckdata.php
+++ enterpaycheckdata.php
@@ -1,66 +1,137 @@
 <?php
 	$menuHighlight = 0;
-	$pageTitle="Report Hours";
+	$pageTitle="Enter Paycheck";
 	include_once("header.php");
+	include_once("UTILITIES/config.php");
+	include_once("UTILITIES/dbutils.php");
 ?>
 
 
 <div class = "col-xs-2"></div> <!-- Used to push jumbotron smaller and to the right -->
 <div class = "col-xs-8">
 <div class="text-center">
+	<div class ="container">
 	<!-- jumbotron-->
 	<div class="jumbotron">
-
 		<div class="text-center">
 			<h1>Enter My Paycheck</h1>
 			<p class="lead">Please enter you paycheck information</p>
-			
-		<!-- need to create form here to send to DB-->
-		<!--<form action="sendHoursdata.php" method="post">-->
+
+		<form action="savePaycheck.php" class="form-horizontal" method="post">
 			<div class="form-group">
-				<div class="input-group">	
-					<div class="input-group-addon">Start Date</div>
-					<input type="date" class="form-control" name="StartDate"><br>
+				<div class="row">
+					<label class="col-sm-2 control-label">Start Date</label>				
+					<div id="datepickerContainer" class="col-sm-8">
 					
-					<div class="input-group-addon">End Date</div>
-					<input type="date" class="form-control" name="EndDate"><br>
+						<!-- DATE PICKER -->
+						    <div class="input-daterange input-group" id="datepicker">
+								<input type="text" class="input-sm form-control" placeholder="MM/DD/YYYY" name="PayPeriodStart" />
+								<span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+								
+								<span class="input-group-addon">to</span>
+								
+								<input type="text" class="input-sm form-control" placeholder="MM/DD/YYYY" name="PayPeriodEnd" />
+								<span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+							</div>
+						<!-- END DATE PICKER-->
+					</div>
 				</div>
 			</div>
 			
-			<div class="form-group">
-				<div class="input-group">	
-					<div class="input-group-addon">Issue Date</div>
-					<input type="date" class="form-control" name="issuedate"/>
+			<!-- not in db, ignored for now
+			
+			<div class="row">
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Issue Date</label>
+					<div id="IssueDateContainer" class="col-sm-8">
+						<div class="input-group date">
+						  <input type="text" class="form-control" placeholder="MM/DD/YYYY" name="IssueDate"><span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+						</div>
+					</div>
+					<!-- Padding 
+					<div class="col-sm-2"></div>
+				</div>
+			</div>
+			-->
+			
+			<div class="row">
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Total Hours</label>
+					<div class="col-sm-8">
+						<input type="number" class="form-control" placeholder="Hours Worked" name="HoursPaid"/>
+					</div>
+					<!-- Padding -->
+					<div class="col-sm-2"></div>
 				</div>
 			</div>
 			
-			<div class="form-group">
-				<div class="input-group">	
-					<div class="input-group-addon">Total Hours</div>
-					<input type="number" class="form-control" placeholder="Hours Worked" name="totalhours"/>
+			<div class="row">
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Employer</label>
+					<div class="col-sm-8" style="color:black">
+						<select name="EmployerID">
+							<!-- Echo out users active employers -->
+							<?php
+								$Username = $_SESSION['Username'];
+								$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
+								// prep query
+								$query = "Select Name,EmployerID from Employers where EmployerID = ANY (Select EmployerID from UsersEmployment where UserID = (Select UserID from Users where Username = '$Username'));";
+									
+								// execute sql statement
+								$result = queryDB($query, $db);
+								
+								// check if it worked
+								if (nTuples($result)> 0) {
+									//output data of each row, 1 is the actual employer id
+									while($row = mysqli_fetch_row($result)) {
+										echo '<option value ="'. $row[1] . '">'. $row[0] .'</option>';
+									}
+								}
+								else{
+									echo "No employers found";
+								}
+							?>	
+						</select>
+					</div>
+					<!-- Padding -->
+					<div class="col-sm-2"></div>
 				</div>
 			</div>
 			
-			<div class="form-group">
-				<div class="input-group">	
-					<div class="input-group-addon">Employer</div>
-					<input type="text" class="form-control" placeholder="Enter Your Employer" name="employer"/>
-				</div>
-			</div>
-			
-			<div class="form-group">
-				<div class="input-group">	
-					<div class="input-group-addon">Amount Paid</div>
-					<input type="number" min="1" step="any" class="form-control"/>
+			<div class="row">
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Amount Paid</label>
+					<div class="col-sm-8">
+						<input type="number" min="1" step="any" placeholder="$x.xx" name="AmountPaid" class="form-control"/>
+					</div>
+					<!-- Padding -->
+					<div class="col-sm-2"></div>
 				</div>
 			</div>
 			
 			<button type="submit" class="btn btn-success btn-lg" name="submit">Report</button>
 		</form>
 		</div>
+		</div>
 	</div> <!-- Jumbotron -->
 </div>
 </div>
+
+<script>
+// This initializes my datepickers based on div id's
+	$('#datepickerContainer .input-daterange').datepicker ({
+    format: "mm/dd/yyyy",
+    todayBtn: "linked",
+    todayHighlight: true
+});
+
+	$('#IssueDateContainer .input-group.date').datepicker ({
+    format: "mm/dd/yyyy",
+    todayBtn: "linked",
+    todayHighlight: true
+});
+
+</script>
 
 <?php
 	include_once("footer.php");
