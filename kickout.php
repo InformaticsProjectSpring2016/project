a863a150b8548881c64e461fed8a8423bea4c5f8
tzhao1 <tianyu-zhao@uiowa.edu>
2016-04-13 12:27:55 -0500

    Update kickout.php

--- kickout.php
+++ kickout.php
@@ -5,7 +5,7 @@
 		// if this is not set, it means they are not logged in
 		header("Location: http://webdev.divms.uiowa.edu/~ngramer/project/");
 		//if the user is not logged in, and we want block the member-only content
-		die("Redirecting to http://webdev.divms.uiowa.edu/~ngramer/project/")
+		die("Redirecting to http://webdev.divms.uiowa.edu/~ngramer/project/");
 	}
 
 ?>
