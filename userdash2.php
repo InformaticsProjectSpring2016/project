d86b63f4adbb049383cb2751df9c4cc137a92e75
EmmaClingman <emmaclingman@gmail.com>
2016-04-22 10:28:36 -0500

    added list of jobs

--- userdash2.php
+++ userdash2.php
@@ -12,9 +12,8 @@
 			<h1>User Dashboard</h1>
 			<div>
 				<form method="link" action="addemployment.php"> 
-					<input type="submit" class="btn btn-success btn-lg" value="Add Another Job">
+					<input type="submit" class="btn btn-danger btn-lg" value="Add Another Job">
 				</form>
-				<!--<button type="submit"  action class="btn btn-success btn-lg" name="submit">Register</button>-->
 			</div>
 			
 			<!--list of user data-->
@@ -39,6 +38,53 @@
 						$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
 						// prep query
 						$query = "Select u.FirstName, p.EntryDate, p.HoursWorked, e.Name from WageDataEntries p INNER JOIN Employers e ON (p.EmployerID = e.EmployerID) INNER JOIN Users u ON (p.UserID = u.UserID);";
+							
+						// execute sql statement
+						$result = queryDB($query, $db);
+						
+						// check if it worked
+						
+						if (nTuples($result)> 0) {
+						//output data of each row
+							while($row = mysqli_fetch_row($result)) {
+								echo "
+								<tr>
+									<th>". $row[0] . "</th>
+									<th>" . $row[1] . "</th> 
+									<th>" . $row[2] . "</th>
+									<th>" . $row[3] . "</th>
+								</tr>";
+							}
+						} else {
+							echo "0 results";
+						}
+
+					?>
+					</tbody>
+				</table>
+		
+		<!--See jobs user has registered-->
+		<<div class = "row">
+				<div class="panel-body">
+				<table class="table table-bordered table-striped">
+				<h3>My Jobs</h3>
+					<!--columns-->
+					<thead>
+					  <tr>
+						<th>Name</th>
+						<th>Location</th>
+						<th>Hourly Wage</th>
+						<th>Standard Hours</th>
+					  </tr>
+					</thead>
+					<!--rows-->
+					<tbody>
+					<?php
+
+						// get a handle to the database
+						$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
+						// prep query
+						$query = "Select e.Name, e.Location, u.HourlyWage, u.StandardHours from Employers e INNER JOIN UsersEmployment u ON (e.EmployerID = u.EmployerID) ORDER BY u.UserID;";
 							
 						// execute sql statement
 						$result = queryDB($query, $db);
