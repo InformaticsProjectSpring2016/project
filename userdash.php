07cbf81e0e68156cd51405f53cf5f79932c977ca
@nate-gramer <nathan-gramer@uiowa.edu>
2016-05-05 01:22:44 -0500

    Fixed issue of broken page layout when no jobs or entries exist

--- userdash.php
+++ userdash.php
@@ -11,27 +11,138 @@
 ?>
 
 <div class = "container">
+
+	<div class="col-md-12">
 	<div class="text-center">
-		<div class="jumbotron">
-			<h1>User Dashboard</h1>
-			<div>
-				<form method="link" action="addemployment.php"> 
-					<input type="submit" class="btn btn-danger btn-lg" value="Add Another Job">
-				</form>
-			</div>
-			
-			<!--list of user data-->
-			<div class = "row">
+		<h1>User Dashboard</h1>
+	</div>
+	</div>
+	<!-- Navigation Buttons -->
+	<div class="col-md-2">
+		<ul class="nav nav-pills nav-stacked marginTop" id="myTabs">
+			<li class="active"><a href="#mydata" data-toggle="pill">My Data</a></li>
+<!-- Echo out users active employers to navbar-->
+<?php
+	$Username = $_SESSION['Username'];
+	$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
+	// prep query
+	$query = "Select Name,EmployerID from Employers where EmployerID = ANY (Select EmployerID from UsersEmployment where UserID = (Select UserID from Users where Username = '$Username'));";
+		
+	// execute sql statement
+	$result = queryDB($query, $db);
+	
+	// check if we have employers and echo nav tabs
+	if (nTuples($result)> 0){
+		while($row = mysqli_fetch_row($result)) {
+			echo '<li><a href="#'.$row[1].'" data-toggle="pill">'. $row[0].'</a></li>';
+		}
+		
+	
+	echo '
+		<form method="link" action="addemployment.php"> 
+			<input type="submit" class="btn btn-danger" value="Add Another Job">
+		</form>
+		</ul>
+	</div><!-- end Nav -->
+	<!-- Begin content container -->
+	<div class="col-md-10">
+	<div class="tab-content">';
+	
+	$result = queryDB($query, $db);
+	// For each employer, echo out a tab container and table
+	while($row = mysqli_fetch_row($result)) {
+		echo'
+			<div class="tab-pane" id="'. $row[1].'">
 				<div class="panel-body">
 				<table class="table table-bordered table-striped">
-				<h3>My Data Entries</h3>
+				<h3>Paycheck entries for '. $row[0] .'</h3>
 					<!--columns-->
 					<thead>
 					  <tr>
-						<th>User Name</th>
-						<th>Entry Date</th>
-						<th>Hours Worked</th>
-						<th>Employer</th>
+						<td>PaycheckID</td>
+						<td>Pay Period Start</td>
+						<td>Pay Period End</td>
+						<td>Hours Paid</td>
+						<td>Hours Worked</td>
+						<td>Amount Paid</td>
+					  </tr>
+					</thead>
+					<!--rows-->
+				<tbody>';
+	
+		// Now, for each paycheck associated with an employer, echo out the corresponding table rows.
+		// prep query
+		$EmployerID = $row[1];
+		//check for hourly entries
+		$UserID = $_SESSION['UserID'];
+		if(nTuples(queryDB("select h.EntryID from WageDataEntries h where h.UserID = '$UserID' and h.EmployerID ='$EmployerID'",$db)) >0){
+			$query = "Select p.PaycheckID, p.PayPeriodStart, p.PayPeriodEnd, p.HoursPaid, p.AmountPaid, SUM(h.HoursWorked), COUNT(h.EntryID) from PaycheckData p INNER JOIN Employers e ON (p.EmployerID = e.EmployerID) INNER JOIN Users u ON (p.UserID = u.UserID) INNER JOIN WageDataEntries h ON (h.UserID = p.UserID) AND (h.EmployerID = p.EmployerID) AND (h.EntryDate >= p.PayPeriodStart) AND (h.EntryDate <= p.PayPeriodEnd) WHERE u.Username = '$Username' and e.EmployerID = '$EmployerID';";
+			// execute sql statement
+			$result2 = queryDB($query, $db);
+			// check for hourly entries within pay range
+			//if(mysqli_fetch_row($result2)[6] != 0){
+				//output data of each row
+				while($row = mysqli_fetch_row($result2)) {
+					echo "
+					<tr>
+						<td>". $row[0] . "</td>
+						<td>" . $row[1] . "</td> 
+						<td>" . $row[2] . "</td>
+						<td>" . $row[3] . "</td>
+						<td>" . $row[5] . "</td>
+						<td>" . $row[4] . "</td>
+					</tr>";
+				
+				} //Close up table inside outer while loop
+				echo'	</tbody>
+						</table>
+						</div>
+					</div>';
+					
+				}/* else{
+					echo "Please add your hours for your entered Pay Periods for $row[0].
+					
+						</tbody>
+						</table>
+						</div>
+					</div>";
+			} */
+		/* } */else{
+			echo "Please add your hours for $row[0].
+			
+				</tbody>
+				</table>
+				</div>
+			</div>";
+		}
+	} // Close employer while loop
+	} //Close if statement checking if we have employers and open what to do if we dont have employers
+	else{
+		echo'
+		<form method="link" action="addemployment.php"> 
+			<input type="submit" class="btn btn-danger" value="Add Another Job">
+		</form>
+		</ul>
+	</div><!-- end Nav -->
+	<!-- Begin content container -->
+	<div class="col-md-10">
+	<div class="tab-content">';
+	}
+	
+	?>
+	<!-- put in mydata container last, make it active -->
+
+	<div class="tab-pane active" id="mydata">
+		<div class="panel-body">
+			<table class="table table-bordered table-striped">
+				<h3>My Hourly Entries</h3>
+					<!--columns-->
+					<thead>
+					  <tr>
+						<td>User Name</td>
+						<td>Entry Date</td>
+						<td>Hours Worked</td>
+						<td>Employer</td>
 					  </tr>
 					</thead>
 					<!--rows-->
@@ -54,10 +165,10 @@
 							while($row = mysqli_fetch_row($result)) {
 								echo "
 								<tr>
-									<th>". $row[0] . "</th>
-									<th>" . $row[1] . "</th> 
-									<th>" . $row[2] . "</th>
-									<th>" . $row[3] . "</th>
+									<td>". $row[0] . "</td>
+									<td>" . $row[1] . "</td> 
+									<td>" . $row[2] . "</td>
+									<td>" . $row[3] . "</td>
 								</tr>";
 							}
 						} else {
@@ -67,54 +178,18 @@
 					?>
 					</tbody>
 				</table>
-		
-		<!--See jobs user has registered-->
-		<div class = "row">
-				<div class="panel-body">
-				<table class="table table-bordered table-striped">
-				<h3>My Jobs</h3>
-					<!--columns-->
-					<thead>
-					  <tr>
-						<th>Name</th>
-						<th>Location</th>
-						<th>Hourly Wage</th>
-						<th>Standard Hours</th>
-					  </tr>
-					</thead>
-					<!--rows-->
-					<tbody>
-					<?php
+		</div> <!-- Panel Body -->
+	</div> <!--tab pane -->
+				
+</div> <!-- End content container -->				
 
-						// get a handle to the database
-						$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
-						// prep query
-						$UserID = $_SESSION['UserID'];
-						$query = "Select e.Name, e.Location, u.HourlyWage, u.StandardHours from Employers e INNER JOIN UsersEmployment u ON (e.EmployerID = u.EmployerID) where u.UserID = '$UserID' ORDER BY u.UserID;";
-							
-						// execute sql statement
-						$result = queryDB($query, $db);
-						
-						// check if it worked
-						
-						if (nTuples($result)> 0) {
-						//output data of each row
-							while($row = mysqli_fetch_row($result)) {
-								echo "
-								<tr>
-									<th>". $row[0] . "</th>
-									<th>" . $row[1] . "</th> 
-									<th>" . $row[2] . "</th>
-									<th>" . $row[3] . "</th>
-								</tr>";
-							}
-						} else {
-							echo "0 results";
-						}
+	
+</div> <!-- End container -->
 
-					?>
-					</tbody>
-				</table>
+
+
+
+
 
 <?php
 	ob_end_flush( );
